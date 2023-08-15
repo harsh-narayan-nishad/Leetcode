efe69239317bb8af0fe6bbbead56595aac0e72c4
Harsh Narayan Nishad <107599048+harsh-narayan-nishad@users.noreply.github.com>
2023-08-15 13:01:34 +0530

    Time: 3 ms (97.98%), Space: 9.4 MB (95%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
+        for(auto ele: nums){
+            ones = (ones^ele) & (~twos);
+            twos = (twos^ele) & (~ones);
+        }
+        return ones;
+    }
+};
