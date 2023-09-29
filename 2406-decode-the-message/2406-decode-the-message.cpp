bd03a7723a872bfa895945b5622b7892e267e546
Harsh Narayan Nishad <107599048+harsh-narayan-nishad@users.noreply.github.com>
2023-09-30 00:45:59 +0530

    Time: 0 ms (100%), Space: 7.2 MB (30.04%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.cpp
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        // Create mapping 
+        //create Mapping 
         char start = 'a';
-        char mapping[280] = {0};
+        char mapping[280] ={0};
 
-        for(auto ch:key){
-            if(ch!= ' ' && mapping[ch] == 0){
+        for(auto ch: key){
+            if(ch != ' ' && mapping[ch] == 0){
                 mapping[ch] = start;
                 start++;
             }
         }
-
         //use mapping
+
         string ans;
-        
         for(auto ch: message){
-            if(ch == ' '){
+            if(ch ==' '){
                 ans.push_back(' ');
             }
             else{
